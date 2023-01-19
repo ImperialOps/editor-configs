0d65efd9ba62d02805d57cc95fc4bf6b5760a1f5
Josh B <josh9398@hotmail.com>
2023-01-19 13:26:19 +0000

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install terraform \
+  infracost \
+  tflint \
+  terraform-docs \
+  infracost \
+  pre-commit \
+  commitizen \
+  go \
+  awscli \
+  aws-iam-authenticator \
+  binutils \
+  kind \
+  helm \
+  skaffold \
+  kubernetes-cli
