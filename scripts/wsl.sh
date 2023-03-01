a36639f16bb081f9c39ca063f491a886b584854b
Josh B <josh9398@hotmail.com>
2023-03-01 23:37:49 +0000

    Create wsl.sh

---
+++ scripts/wsl.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# powershell: winget install Neovim.Neovim
+
+# Get system clipboard working
+sudo ln -s "/mnt/c/Program\ Files/Neovim/bin/win32yank.exe" "/usr/local/bin/win32yank.exe"
