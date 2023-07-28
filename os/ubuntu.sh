cef4f4f92e0bf1a48dd623926b1ddda9f7ade586
Josh B <josh9398@hotmail.com>
2023-07-28 15:46:59 +0100

    Update ubuntu.sh

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -11,6 +11,7 @@ sudo apt install -y \
 
 git config --global user.name "Josh B"
 git config --global user.email "josh9398@hotmail.com"
+git config --global init.defaultBranch "main"
 
 # install mini-conda
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
