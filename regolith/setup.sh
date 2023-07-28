8164d6490719a2707d2c5202020cac0f66eaf5ca
Josh B <josh9398@hotmail.com>
2023-07-28 09:34:59 +0100

    Update setup.sh

--- regolith/setup.sh
+++ regolith/setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+sudo apt update
+
+sudo apt install -y \
+    gnome-screenshot \
+    xclip
+# Set keyboard shortcut
+# Settings -> Keyboard -> View and Customize Shortcuts -> Custom Shortcuts
+# sh -c "gnome-screenshot -c -a -f /tmp/test && cat /tmp/test | xclip -i -selection clipboard -target image/png"
+
 sudo apt install -y \
     i3xrocks-rofication \
     i3xrocks-cpu-usage \
@@ -11,4 +20,3 @@ sudo apt install -y regolith-look-*
 sudo apt remove -y i3xrocks-net-traffic
 
 echo "i3-wm.bar.position:  top" >> ~/.config/regolith2/Xresources
-
