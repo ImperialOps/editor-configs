f233fc50a2b3c3c5f58bc8117332c21eb1d6ced1
Josh B <josh9398@hotmail.com>
2023-08-05 22:06:45 +0100

    Update setup.sh

--- regolith/setup.sh
+++ regolith/setup.sh
@@ -9,6 +9,7 @@ sudo apt install -y \
 # Settings -> Keyboard -> View and Customize Shortcuts -> Custom Shortcuts > Ctrl + Shift + Print
 # sh -c "gnome-screenshot -c -a -f /tmp/test && cat /tmp/test | xclip -i -selection clipboard -target image/png"
 
+# Regolith install : https://regolith-desktop.com/
 sudo apt install -y \
     regolith-compositor-picom-glx \
     i3xrocks-rofication \
