413747510026c5079ae10857767e9ca13834fc40
Josh B <josh9398@hotmail.com>
2023-08-05 22:17:12 +0100

    Update colours.lua

--- nvim/after/plugin/colours.lua
+++ nvim/after/plugin/colours.lua
@@ -1,1 +1,8 @@
+require('rose-pine').setup({
+	--- @usage 'auto'|'main'|'moon'|'dawn'
+	variant = 'moon',
+	--- @usage 'main'|'moon'|'dawn'
+	dark_variant = 'moon',
+})
+
 vim.cmd.colorscheme 'rose-pine'
